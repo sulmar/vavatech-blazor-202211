5cbb6e26c6b385cb574e9d7ee786d306cd6c4fe6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-11-28 18:07:40 +0100

    Refaktoring - nazwany klient HttpClient

--- src/Vavatech.Shopper.ClientApp/Program.cs
+++ src/Vavatech.Shopper.ClientApp/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Vavatech.Shopper.ClientApp;
+using Vavatech.Shopper.ClientApp.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,6 +9,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+
+// dotnet add package Microsoft.Extensions.Http --version 6.0.0
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://jsonplaceholder.typicode.com") });
+
+// Rejestracja nazwanego klienta Http (Named HttpClient)
+builder.Services.AddHttpClient<JsonPlaceholderService>(sp =>
+{
+    sp.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+});
 
 await builder.Build().RunAsync();
---
+++ src/Vavatech.Shopper.ClientApp/Services/JsonPlaceholderService.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http.Json;
+using Vavatech.Shopper.Domain;
+
+namespace Vavatech.Shopper.ClientApp.Services
+{
+    public class JsonPlaceholderService
+    {
+        private readonly HttpClient client;
+        public JsonPlaceholderService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await client.GetFromJsonAsync<IEnumerable<User>>("/users");
+        }
+    }
+}

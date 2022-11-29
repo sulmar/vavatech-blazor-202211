9e46b24a92670a84ebba721eae205e88ca390b3d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-11-29 12:30:36 +0100

    Dodanie pobierania produktów do Api

--- src/Vavatech.Shopper.Api/Program.cs
+++ src/Vavatech.Shopper.Api/Program.cs
@@ -1,11 +1,20 @@
-var app = WebApplication.Create(args);
+using Vavatech.Shopper.Domain;
+using Vavatech.Shopper.Infrastructure;
+
+// var app = WebApplication.Create(args);
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
+
+var app = builder.Build();
+
 app.MapGet("/", () => "Hello World!");
 
 // GET api/products
-app.MapGet("api/products", () => "Hello Products");
+app.MapGet("api/products", async (IProductRepository repository) => await repository.GetAllAsync());
 
 // GET api/products/{id}
-app.MapGet("api/products/{id:int:min(1)}", (int id) => $"Hello Product {id}");
+app.MapGet("api/products/{id:int:min(1)}", async (int id, IProductRepository repository) => await repository.GetById(id));
 
 
 //app.MapGet("api/users", (HttpRequestMessage req, HttpResponseMessage res) 

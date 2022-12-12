eac05eb76f02520f37e6d37d3861efce525357d8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-12 15:59:16 +0100

    Dodanie walidacji DataAnnotations

--- src/Vavatech.Shopper.Domain/Product.cs
+++ src/Vavatech.Shopper.Domain/Product.cs
@@ -1,10 +1,14 @@
-﻿namespace Vavatech.Shopper.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vavatech.Shopper.Domain;
 
 public class Product : BaseEntity
 {
+    [Required, StringLength(50, MinimumLength = 3, ErrorMessage = "Długość nieprawidłowa")]
     public string Name { get; set; }
     public string Description { get; set; }
     public string Color { get; set; }
+    [Range(1, 1000)]
     public decimal Price { get; set; }
     public bool HasDiscount { get; set; }
     public decimal? Discount { get; set; }

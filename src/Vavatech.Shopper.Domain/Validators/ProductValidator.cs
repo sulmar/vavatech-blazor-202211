51a21325fd9d72a792d20309714caf9863f35b01
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-12 16:30:00 +0100

    Dodanie walidacji z użyciem FluentValidations

---
+++ src/Vavatech.Shopper.Domain/Validators/ProductValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vavatech.Shopper.Domain.Validators
+{
+    // dotnet add package FluentValidation
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator() 
+        { 
+            RuleFor(p=>p.Name)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(50);
+
+            RuleFor(p => p.Description)
+                .NotEmpty();
+
+            RuleFor(p => p.Price)
+                .InclusiveBetween(1, 1000);
+
+            RuleFor(p => p.Discount)
+                .InclusiveBetween(1, 500)
+                .When(p => p.HasDiscount);
+        }
+    }
+}
